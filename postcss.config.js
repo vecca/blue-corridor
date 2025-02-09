079b93f257e816efeee1850251d73ed1725b5361
Vedran Knezevic <knezevic.vedran@gmail.com>
2025-02-09 18:36:20 +1000

    Added new sections and styles

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,7 @@ module.exports = {
   plugins: [
     require("postcss-mixins"),
     require("postcss-import"), // Resolves @import statements
+    require("postcss-nested"),
     require("autoprefixer"), // Automatically add vendor prefixes
     require("postcss-simple-vars"), // Enables CSS variables
     require("postcss-preset-env")({
--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,3 @@ import "../css/main.css"; // Import the main CSS file
 
 console.log("CSS and JS are integrated!");
 console.log("PostCSS is working!");
-
-let jsHook = document.querySelector(".jsHook");
-jsHook.innerHTML = "Some Text directly from js!! REALY Amazing Stuff";
