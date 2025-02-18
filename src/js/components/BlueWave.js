6447f8818a4a126137875408814d0589bfb3dde1
Vedran Knezevic <knezevic.vedran@gmail.com>
2025-02-18 21:33:18 +1000

    Ajusted scrolling and some css in general

--- src/js/components/BlueWave.js
+++ src/js/components/BlueWave.js
@@ -1,4 +1,4 @@
-const svg = document.querySelector("svg.squiggle");
+/* const svg = document.querySelector("svg.squiggle");
 const path = svg.querySelector("path");
 
 const scroll = () => {
@@ -11,6 +11,28 @@ const scroll = () => {
 
   path.style.strokeDasharray = `${pathLength}`;
   path.style.strokeDashoffset = `${pathLength * (1 - percentage)}`;
+};
+
+scroll();
+window.addEventListener("scroll", scroll); */
+
+/* Code version for multiple paths */
+const svg = document.querySelector("svg.squiggle");
+const paths = svg.querySelectorAll("path");
+console.log(paths);
+
+const scroll = () => {
+  const distance = window.scrollY;
+  const totalDistance = svg.clientHeight - window.innerHeight;
+  const percentage = distance / totalDistance;
+
+  for (var i = 0; i < paths.length; i++) {
+    let path = paths[i];
+    //console.log(path);
+    const pathLength = path.getTotalLength();
+    path.style.strokeDasharray = `${pathLength}`;
+    path.style.strokeDashoffset = `${pathLength * (1 - percentage)}`;
+  }
 };
 
 scroll();
