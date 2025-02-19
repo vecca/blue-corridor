27535c9bfe6647a60c1150ba1f1a420c3d046ede
Vedran Knezevic <knezevic.vedran@gmail.com>
2025-02-19 21:07:21 +1000

    Tweaked the animation, intro text and css

--- src/js/components/BlueWave.js
+++ src/js/components/BlueWave.js
@@ -17,7 +17,7 @@ scroll();
 window.addEventListener("scroll", scroll); */
 
 /* Code version for multiple paths */
-const svg = document.querySelector("svg.squiggle");
+/* const svg = document.querySelector("svg.squiggle");
 const paths = svg.querySelectorAll("path");
 console.log(paths);
 
@@ -29,6 +29,26 @@ const scroll = () => {
   for (var i = 0; i < paths.length; i++) {
     let path = paths[i];
     //console.log(path);
+    const pathLength = path.getTotalLength();
+    path.style.strokeDasharray = `${pathLength}`;
+    path.style.strokeDashoffset = `${pathLength * (1 - percentage)}`;
+  }
+};
+
+scroll();
+window.addEventListener("scroll", scroll); */
+
+/* Version 3 */
+const svg = document.querySelector("svg.squiggle");
+const paths = svg.querySelectorAll("path");
+
+const scroll = () => {
+  const distance = window.scrollY;
+  const totalDistance = svg.clientHeight - window.innerHeight;
+  const percentage = distance / totalDistance / 2; // Adjust the factor to slow down the animation
+
+  for (var i = 0; i < paths.length; i++) {
+    let path = paths[i];
     const pathLength = path.getTotalLength();
     path.style.strokeDasharray = `${pathLength}`;
     path.style.strokeDashoffset = `${pathLength * (1 - percentage)}`;
